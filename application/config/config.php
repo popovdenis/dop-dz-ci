ee01539b7105e757b4a47d6669c011bc658fec0b
denis.popov <denis.popov@symmetrics.de>
2016-02-06 23:51:59 +0200

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://dop-dz-ci/';
+$config['base_url'] = 'http://dop-dz-ci:8080/';
 
 /*
 |--------------------------------------------------------------------------
@@ -155,7 +155,7 @@ $config['charset'] = 'UTF-8';
 | setting this variable to TRUE (boolean).  See the user guide for details.
 |
 */
-$config['enable_hooks'] = FALSE;
+$config['enable_hooks'] = true;
 
 /*
 |--------------------------------------------------------------------------
--- tests/application/controllers/ArticleTest.php
+++ tests/application/controllers/ArticleTest.php
@@ -1,4 +1,5 @@
 <?php
+require_once("RemoteConnect.php");
 /**
  * Created by PhpStorm.
  * User: Denis
@@ -7,8 +8,25 @@
  */
 class ArticleTest extends PHPUnit_Framework_TestCase
 {
-    public function test_add()
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    public function testAdd()
     {
         $this->assertEquals(1, 1);
+    }
+
+    public function testConnectionIsValid()
+    {
+        // проверка валидности соединения с сервером
+        $connObj = new RemoteConnect();
+        $serverName = 'www.google.com';
+        $this->assertTrue($connObj->connectToServer($serverName) !== false);
     }
 }

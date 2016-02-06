59e1761ac8dc49ec8c25148e1080edda63b2d32a
Denis <monah40@gmail.com>
2016-02-06 21:00:47 +0200

    all project

---
+++ tests/application/controllers/ArticleTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Denis
+ * Date: 06.02.2016
+ * Time: 12:33
+ */
+class ArticleTest extends PHPUnit_Framework_TestCase
+{
+    public function test_add()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
